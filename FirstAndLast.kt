e174494b997148fc13a8b4b471279f236e93a930
spacemankz <zharmukhamet2@gmail.com>
2022-05-22 18:10:56 +0600

    34. Find First and Last Position of Element in Sorted Array

---
+++ FirstAndLast.kt
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ */
+
+class FirstAndLast {
+
+        fun searchRange(nums: IntArray, target: Int): IntArray {
+            if (nums.size == 0) return intArrayOf(-1, -1)
+
+            var l: Int = 0
+            var r: Int = nums.size-1
+            var mid = 0
+            var res = IntArray(2)
+
+            while (l < r) {
+                mid = (l+r) / 2
+                if (nums[mid] < target) {
+                    l = mid+1
+                } else {
+                    r = mid
+                }
+            }
+            if (nums[l] != target) return intArrayOf(-1, -1)
+            res[0] = l
+
+            r = nums.size-1
+
+            while (l < r) {
+                mid = (l+r) / 2 + 1
+                if (nums[mid] == target) {
+                    l = mid
+                } else {
+                    r = mid-1
+                }
+            }
+            res[1] = l
+            return res
+        }
+
+
+}
